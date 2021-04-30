2746ae0e84d8222a78b7f22064faa1125b343d34
Emre Rothzerg <hello@emrerothzerg.com>
2021-05-01 00:19:46 +1000

    ect var added

--- .buildkite/scripts/upload-docker-image.sh
+++ .buildkite/scripts/upload-docker-image.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+ECR_NAME="071035219568.dkr.ecr.us-east-1.amazonaws.com/emre-buildkite-test"
+
 aws ecr get-login-password \
     --region us-east-1 \
 | docker login \
     --username AWS \
-    --password-stdin 071035219568.dkr.ecr.us-east-1.amazonaws.com/emre-buildkite-test
+    --password-stdin $ECR_NAME
 
-docker build -t 071035219568.dkr.ecr.us-east-1.amazonaws.com/emre-buildkite-test .
+docker build -t $ECR_NAME .
 
-docker push 071035219568.dkr.ecr.us-east-1.amazonaws.com/emre-buildkite-test
+docker push $ECR_NAME
